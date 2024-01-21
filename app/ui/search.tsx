cd07d689361bb153ad1fb74bb4a577a4327f444b
Artem Chernii <artemcherniy7@gmail.com>
2024-01-21 11:39:46 +0000

    feat: add pagination and search for invoices page

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,43 @@
 'use client';
-
+import { useSearchParams, useRouter, usePathname } from 'next/navigation';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useDebouncedCallback } from 'use-debounce';
+
+type AnyFunction = (...args: any[]) => void;
+
+function debounce<T extends AnyFunction>(func: T, delay: number): T {
+  let timeoutId: ReturnType<typeof setTimeout> | null;
+
+  return function (...args: Parameters<T>): void {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+
+    timeoutId = setTimeout(() => {
+      func(args);
+      timeoutId = null;
+    }, delay);
+  } as T;
+}
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const handleSearch = debounce((value: string) => {
+    const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
+
+    if (value) {
+      params.set('query', value);
+    } else {
+      params.delete('query');
+    }
+
+    router.replace(`${pathname}?${params.toString()}`);
+  }, 500);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +46,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e) => handleSearch(e.target.value)}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
